97468c95aa5e4da735f954520bcf654e6dfec6f8
Rama Krishnam Raju Kalidindi <krishna@Ramas-MacBook-Pro.attlocal.net>
2026-05-03 10:18:23 -0400

    Streams

---
+++ src/05StreamsAPI/StreamsExample.java
@@ -0,0 +1,22 @@
+import data.StudentDatabase;
+import data.Student;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.function.Predicate;
+
+class StreamsExample {
+  public static void main(String[] args) {
+
+    Predicate<Student> gradeLevelGreaterThanEqualToThree = student -> student.getGradeLevel() >= 3;
+    Predicate<Student> gpaGreaterThanEqualToThreePointNine = student -> student.getGpa() >= 3.9;
+
+    Map<String, List<String>> studentActivitiesMap = StudentDatabase.getAllStudents().parallelStream()
+      .filter(gradeLevelGreaterThanEqualToThree)
+      .filter(gpaGreaterThanEqualToThreePointNine)
+      .collect(Collectors.toMap(Student::getName, Student::getActivities));
+
+    System.out.println(studentActivitiesMap);
+
+  }
+}
